ec3db491b9687d0b65fb7328c92cee39b4d66ba7
David Richards <davidlamontrichards@gmail.com>
2013-08-12 16:09:00 -0600

    Add helper-method version of the ListsController.
    Want to use helper methods instead of bare
      instance variables.  This was to teach
      the principle of how to keep your thinking
      clean with controllers, and how the controller
      and view depend on each other.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,26 +1,22 @@
 class ListsController < ApplicationController
 
-  helper_method :entries
+  helper_method :entries, :list, :lists
 
   # GET /lists
   # GET /lists.json
   def index
-    @lists = List.all
-
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @lists }
+      format.json { render json: lists }
     end
   end
 
   # GET /lists/1
   # GET /lists/1.json
   def show
-    @list = List.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @list }
+      format.json { render json: list }
     end
   end
 
@@ -31,13 +27,12 @@ class ListsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @list }
+      format.json { render json: list }
     end
   end
 
   # GET /lists/1/edit
   def edit
-    @list = List.find(params[:id])
   end
 
   # POST /lists
@@ -46,12 +41,12 @@ class ListsController < ApplicationController
     @list = List.new(params[:list])
 
     respond_to do |format|
-      if @list.save
-        format.html { redirect_to @list, notice: 'List was successfully created.' }
-        format.json { render json: @list, status: :created, location: @list }
+      if list.save
+        format.html { redirect_to list, notice: 'List was successfully created.' }
+        format.json { render json: list, status: :created, location: list }
       else
         format.html { render action: "new" }
-        format.json { render json: @list.errors, status: :unprocessable_entity }
+        format.json { render json: list.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -59,15 +54,13 @@ class ListsController < ApplicationController
   # PUT /lists/1
   # PUT /lists/1.json
   def update
-    @list = List.find(params[:id])
-
     respond_to do |format|
-      if @list.update_attributes(params[:list])
-        format.html { redirect_to @list, notice: 'List was successfully updated.' }
+      if list.update_attributes(params[:list])
+        format.html { redirect_to list, notice: 'List was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @list.errors, status: :unprocessable_entity }
+        format.json { render json: list.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -75,8 +68,7 @@ class ListsController < ApplicationController
   # DELETE /lists/1
   # DELETE /lists/1.json
   def destroy
-    @list = List.find(params[:id])
-    @list.destroy
+    list.destroy
 
     respond_to do |format|
       format.html { redirect_to lists_url }
@@ -86,12 +78,16 @@ class ListsController < ApplicationController
 
   protected
 
+  def lists
+    @lists ||= List.all
+  end
+
   def list
-    List.find(params[:id])
+    @list ||= List.find(params[:id])
   end
 
   def entries
-    list.entries
+    @entries ||= list.entries
   end
 
 end
