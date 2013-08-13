891c820c9ddc7a87ffc1b994e91cc60c90a72de4
David Richards <davidlamontrichards@gmail.com>
2013-08-12 18:01:14 -0600

    Nest entries under list.
    This means that we don't have a master shopping list
      right now (we'll need to add that later).
    This means that we always know what the list_id should
      be whenever we deal with an entry.
    We don't have a good way to link the item yet, that's
      about to happen.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,86 +1,93 @@
 class EntriesController < ApplicationController
-  # GET /entries
-  # GET /entries.json
-  def index
-    @entries = Entry.all
 
+  helper_method :list, :entry, :entries
+
+  # GET /list/:list_id/entries
+  # GET /list/:list_id/entries.json
+  def index
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @entries }
+      format.json { render json: entries }
     end
   end
 
-  # GET /entries/1
-  # GET /entries/1.json
+  # GET /list/:list_id/entries/1
+  # GET /list/:list_id/entries/1.json
   def show
-    @entry = Entry.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @entry }
+      format.json { render json: entry }
     end
   end
 
-  # GET /entries/new
-  # GET /entries/new.json
-  def new(list)
-    @entry = Entry.new
-    @list = list
+  # GET /list/:list_id/entries/new
+  # GET /list/:list_id/entries/new.json
+  def new
+    @entry = list.entries.build
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @entry }
+      format.json { render json: entry }
     end
   end
 
-  # GET /entries/1/edit
+  # GET /list/:list_id/entries/1/edit
   def edit
-    @entry = Entry.find(params[:id])
-    @list = @entry.list
   end
 
-  # POST /entries
-  # POST /entries.json
+  # POST /list/:list_id/entries
+  # POST /list/:list_id/entries.json
   def create
-    @entry = Entry.new(params[:entry])
+    @entry = list.entries.build(params[:entry])
 
     respond_to do |format|
-      if @entry.save
-        format.html { redirect_to @entry, notice: 'Entry was successfully created.' }
-        format.json { render json: @entry, status: :created, location: @entry }
+      if entry.save
+        format.html { redirect_to [list, entry], notice: 'Entry was successfully created.' }
+        format.json { render json: entry, status: :created, location: [list, entry] }
       else
         format.html { render action: "new" }
-        format.json { render json: @entry.errors, status: :unprocessable_entity }
+        format.json { render json: entry.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PUT /entries/1
-  # PUT /entries/1.json
+  # PUT /list/:list_id/entries/1
+  # PUT /list/:list_id/entries/1.json
   def update
-    @entry = Entry.find(params[:id])
-
     respond_to do |format|
-      if @entry.update_attributes(params[:entry])
-        @list = List.find(@entry.list_id)
-        format.html { redirect_to @list, notice: 'Entry was successfully updated.' }
+      if entry.update_attributes(params[:entry])
+        format.html { redirect_to list, notice: 'Entry was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @entry.errors, status: :unprocessable_entity }
+        format.json { render json: entry.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /entries/1
-  # DELETE /entries/1.json
+  # DELETE /list/:list_id/entries/1
+  # DELETE /list/:list_id/entries/1.json
   def destroy
-    @entry = Entry.find(params[:id])
-    @entry.destroy
+    entry.destroy
 
     respond_to do |format|
-      format.html { redirect_to entries_url }
+      format.html { redirect_to list_entries_url(list) }
       format.json { head :no_content }
     end
   end
+
+  protected
+
+  def entries
+    @entries = list.entries
+  end
+
+  def entry
+    @entry ||= list.entries.find(params[:id])
+  end
+
+  def list
+    @list ||= List.find(params[:list_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Shopper::Application.routes.draw do
 
-  resources :entries
   resources :lists do
     resources :items
+    resources :entries
   end
 
   # The priority is based upon order of creation:
