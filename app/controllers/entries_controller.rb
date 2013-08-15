f985c52fd36595022e4b6d97679a0940123dff02
David Richards <davidlamontrichards@gmail.com>
2013-08-14 18:07:19 -0600

    Fix the find or create logic on entry items.
    This is step 4 of 4.
    Uses the ActiveRecord autosave: true option, which calls
      autosave_associated_records_for_item.  That method looks for
      an item of the same name and uses that if found.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -44,7 +44,7 @@ class EntriesController < ApplicationController
 
     respond_to do |format|
       if entry.save
-        format.html { redirect_to [list, entry], notice: 'Entry was successfully created.' }
+        format.html { redirect_to list, notice: 'Entry was successfully created.' }
         format.json { render json: entry, status: :created, location: [list, entry] }
       else
         format.html { render action: "new" }
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,9 +1,17 @@
 class Entry < ActiveRecord::Base
   attr_accessible :bought, :item_id, :list_id, :quantity, :item_attributes
 
-  belongs_to :list
+  belongs_to :list, :autosave => true
   belongs_to :item
 
   accepts_nested_attributes_for :item, allow_destroy: true, reject_if: :all_blank
+
+  def autosave_associated_records_for_item
+    if new_item = Item.find_by_name(item.name)
+      self.item = new_item
+    end
+    self.item.save!
+    self.item_id = self.item.id
+  end
 
 end
